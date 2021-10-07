fea45e2421815c8a87f34e2d6da78e2b50ddfd62
mojjan4 <christopher.persson91@gmail.com>
2021-10-07 23:45:02 +0200

    .

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,49 @@
 import Head from 'next/head'
+import Banner from '../components/Banner'
+import Header from '../components/Header'
+import SmallCard from '../components/SmallCard';
 
-export default function Home() {
+export default function Home({exploreData}) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Airbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Let's do Airbnb</h1>
+      <Header/>     
+      <Banner/>   
+      
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({img, distance, location}) => (
+              <SmallCard 
+                key={img}
+                img={img} 
+                distance={distance} 
+                location={location}
+              />
+            ))}
+          </div>
+          
+        </section>
+      </main>   
+  {/* Footer */}
     </div>
   )
+}
+
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp')
+    .then((res) => res.json()
+    );
+    return {
+      props: {
+        exploreData
+      }
+    }
 }
