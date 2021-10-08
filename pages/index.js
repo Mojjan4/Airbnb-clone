f001e557ec137d7cfd48961602911aee73cb6a41
mojjan4 <christopher.persson91@gmail.com>
2021-10-08 19:07:57 +0200

    First side done

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard';
+import LargeCard from '../components/LargeCard';
+import Footer from '../components/Footer';
 
-export default function Home({exploreData}) {
+export default function Home({exploreData, cardsData}) {
   return (
     <div className="">
       <Head>
@@ -11,7 +14,8 @@ export default function Home({exploreData}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header/>     
+      <Header/>
+           
       <Banner/>   
       
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
@@ -30,10 +34,32 @@ export default function Home({exploreData}) {
           </div>
           
         </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({img, title}) => (
+              <MediumCard 
+                key={img} 
+                img={img} 
+                title={title}
+              />
+            ))}
+          </div>
+        </section> 
+
+        <LargeCard 
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb"
+          buttonText="Get Inspired"
+        />
       </main>   
-  {/* Footer */}
+
+      <Footer />
     </div>
-  )
+  );
 }
 
 
@@ -41,9 +67,15 @@ export async function getStaticProps() {
   const exploreData = await fetch('https://links.papareact.com/pyp')
     .then((res) => res.json()
     );
+
+  const cardsData = await fetch('https://links.papareact.com/zp1')
+    .then((res) => res.json()
+    );
+  
     return {
       props: {
-        exploreData
-      }
-    }
+        exploreData,
+        cardsData
+      },
+    };
 }
